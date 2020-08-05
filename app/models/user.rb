eae9dd464df2cbb0cb836876b01b2980a9b3936b
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-08-05 19:24:51 +0900

    正規表現の変数化、パスワードの文字数のバリデーションを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,27 +8,31 @@ class User < ApplicationRecord
             :family_name_kana, :first_name_kana, :birthday,
               presence: true
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
               uniqueness: true,
-              format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+              format: { with: VALID_EMAIL_REGEX }
 
+  VALID_PASSWORD_REGEX = /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./
   validates :password,
-              length: { minimum: 6 },
-              format: { with: /(?=.*\d+.*)(?=.*[a-zA-Z]+.*)./,
+              format: { with: VALID_PASSWORD_REGEX,
                         message: "include both letters and numbers" }
-  validates :family_name,
-              format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
-                        message: "Full-width characters" }
-                        
-  validates :family_name_kana,
-              format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/,
-                        message: "Full-width katakana characters"}
 
+  VALID_ZENKAKU_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+  validates :family_name,
+              format: { with: VALID_ZENKAKU_REGEX,
+                        message: "Full-width characters" }
+  validates :first_name,
+              format: { with: VALID_ZENKAKU_REGEX,
+                        message: "Full-width characters" }
+
+  VALID_ZENKAKU_KANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
+  validates :family_name_kana,
+              format: { with: VALID_ZENKAKU_KANA_REGEX,
+                        message: "Full-width katakana characters"}
   validates :first_name_kana,
-              format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/,
+              format: { with: VALID_ZENKAKU_KANA_REGEX,
                         message: "Full-width katakana characters" }
 
-  validates :first_name,
-              format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,
-                        message: "Full-width characters" }
+  
 end
