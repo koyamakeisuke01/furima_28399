12a16802690bf78927a46e0f5537c6c0a268376a
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-08-06 19:13:51 +0900

    ログインユーザーのみ出品ページへ遷移可能に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    # 未ログインユーザーはログインページへリダイレクト
+    login_check
+
+    
+  end
+
+  private
+  def login_check
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 end
