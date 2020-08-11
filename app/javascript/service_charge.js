04a5129e7b4878ab9f34d8ba90cd29637d51bf2e
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-08-11 13:34:14 +0900

    販売手数料と販売利益の計算を実装

---
+++ app/javascript/service_charge.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+
+  const taxRate = 0.1;
+  const price = document.getElementById("item_price");
+  const taxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  price.addEventListener('change', function() {
+    taxPrice.value = price.value * taxRate;
+    taxPrice.innerHTML = price.value * taxRate;
+    profit.innerHTML = price.value - taxPrice.value;
+  })
+
+})
