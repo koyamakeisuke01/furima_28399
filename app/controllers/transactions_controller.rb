68528b3caa820acfbdb055751893a97041d16efd
koyamakeisuke01 <koyamakeisuke01@gmail.com>
2020-08-18 17:59:50 +0900

    商品購入機能の実装

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,6 +5,17 @@ class TransactionsController < ApplicationController
     seller_check
   end
 
+  def create    
+    @transaction = ItemTransaction.new(transaction_params)
+    if @transaction.valid?      
+      pay_item
+      @transaction.save
+      return redirect_to root_path
+    else
+      render :index
+    end
+  end
+
   private
   def login_check
     unless user_signed_in?
@@ -17,6 +28,20 @@ class TransactionsController < ApplicationController
     if current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def transaction_params
+    params.permit(:token, :zip_code, :shipping_address_id, :city, :address, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = "sk_test_f82bef18daaf141bd8040342"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: transaction_params[:token],
+      currency:'jpy'
+    )
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :shipping_address
   belongs_to_active_hash :shipping_date
-  has_one :order
+  has_one :destination
 
   # バリデーション
   with_options presence: true do
